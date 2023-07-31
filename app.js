e0ec0ee0fcaeda765ab2fd0b7b1b73ec0c2242b3
Devendra072002 <patildevendra07072002@gmail.com>
2023-08-01 01:12:17 +0530

    routes updated.

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 
   // production script
   app.use(express.static("./client/build"));
-  app.get("*",(req,res)=>{
+  app.get("/",(req,res)=>{
     res.sendFile(path.resolve(__dirname,"client","build","index.html"))
   });
 
